eca4e406ce4000165122e59f2c242b9cc0c82459
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-05-18 20:14:44 +0200

    erase unnecesary code

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
-// Import firebase-admin
-import * as admin from 'firebase-admin';
-import { ServiceAccount } from 'firebase-admin';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
 
-  // Set the config options
-
-  const adminConfig: ServiceAccount = {
-    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
-    privateKey: (
-      configService.get<string>('FIREBASE_PRIVATE_KEY') ?? ''
-    ).replace(/\\n/g, '\n'),
-    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-  };
-
-  // Initialize the firebase admin app
-
-  admin.initializeApp({
-    credential: admin.credential.cert(adminConfig),
-  });
   await app.listen(3000);
 }
 bootstrap();

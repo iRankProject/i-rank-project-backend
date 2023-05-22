4e711a70cb389e71c8752801c3aa252b0396b3e6
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-05-22 16:38:43 +0200

    graphql types for the graphql schemas

---
+++ src/graphql.ts
@@ -0,0 +1,103 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export class CreateCategoryInput {
+    exampleField?: Nullable<number>;
+}
+
+export class UpdateCategoryInput {
+    id: number;
+}
+
+export class CreatePostInput {
+    exampleField?: Nullable<number>;
+}
+
+export class UpdatePostInput {
+    id: number;
+}
+
+export class CreateUserInput {
+    exampleField?: Nullable<number>;
+}
+
+export class UpdateUserInput {
+    id: number;
+}
+
+export class CreateVoteInput {
+    exampleField?: Nullable<number>;
+}
+
+export class UpdateVoteInput {
+    id: number;
+}
+
+export class Category {
+    exampleField?: Nullable<number>;
+}
+
+export abstract class IQuery {
+    abstract categories(): Nullable<Category>[] | Promise<Nullable<Category>[]>;
+
+    abstract category(id: number): Nullable<Category> | Promise<Nullable<Category>>;
+
+    abstract posts(): Nullable<Post>[] | Promise<Nullable<Post>[]>;
+
+    abstract post(id: number): Nullable<Post> | Promise<Nullable<Post>>;
+
+    abstract users(): Nullable<User>[] | Promise<Nullable<User>[]>;
+
+    abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract votes(): Nullable<Vote>[] | Promise<Nullable<Vote>[]>;
+
+    abstract vote(id: number): Nullable<Vote> | Promise<Nullable<Vote>>;
+}
+
+export abstract class IMutation {
+    abstract createCategory(createCategoryInput: CreateCategoryInput): Category | Promise<Category>;
+
+    abstract updateCategory(updateCategoryInput: UpdateCategoryInput): Category | Promise<Category>;
+
+    abstract removeCategory(id: number): Nullable<Category> | Promise<Nullable<Category>>;
+
+    abstract createPost(createPostInput: CreatePostInput): Post | Promise<Post>;
+
+    abstract updatePost(updatePostInput: UpdatePostInput): Post | Promise<Post>;
+
+    abstract removePost(id: number): Nullable<Post> | Promise<Nullable<Post>>;
+
+    abstract createUser(createUserInput: CreateUserInput): User | Promise<User>;
+
+    abstract updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+
+    abstract removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract createVote(createVoteInput: CreateVoteInput): Vote | Promise<Vote>;
+
+    abstract updateVote(updateVoteInput: UpdateVoteInput): Vote | Promise<Vote>;
+
+    abstract removeVote(id: number): Nullable<Vote> | Promise<Nullable<Vote>>;
+}
+
+export class Post {
+    exampleField?: Nullable<number>;
+}
+
+export class User {
+    exampleField?: Nullable<number>;
+}
+
+export class Vote {
+    exampleField?: Nullable<number>;
+}
+
+type Nullable<T> = T | null;

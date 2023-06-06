7a1929184fd5932c88e39aadc0cb7c929bee971f
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-06-06 18:38:46 +0200

    erase the modules

--- src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
-import { PrismaService } from '../../prisma/prisma.service';
-
-@Module({
-  providers: [PrismaService, UsersResolver, UsersService],
-})
-export class UsersModule {}
--- src/votes/votes.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VotesService } from './votes.service';
-import { VotesResolver } from './votes.resolver';
-
-@Module({
-  providers: [VotesResolver, VotesService]
-})
-export class VotesModule {}

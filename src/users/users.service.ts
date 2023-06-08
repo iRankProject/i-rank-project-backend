eb6c3998005bca34ef27de2e4353e4f820db99ee
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-06-08 14:31:39 +0200

    add the create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 import { CreateOneUserArgs } from 'src/@generated/user/create-one-user.args';
+import { encodePassword } from '../utils/bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateOneUserArgs) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+  async create(createUserInput: CreateOneUserArgs) {
+    const { username, email, password } = createUserInput.data;
+
+    const hashedPassword = encodePassword(password);
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ username: { equals: username } }, { email: { equals: email } }],
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Username or email already exists');
+    }
+
+    try {
+      return await this.prisma.user.create({
+        data: {
+          ...createUserInput.data,
+          password: hashedPassword,
+        },
+      });
+    } catch (error) {
+      throw new Error('Unable to create user');
+    }
   }
 
   findAll() {

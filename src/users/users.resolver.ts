61ca9577638780020e511eb87511129e7dc56164
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-06-23 17:18:27 +0200

    erase unnecesary code

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from '../@generated/user/user.model';
 import { CreateOneUserArgs } from '../@generated/user/create-one-user.args';

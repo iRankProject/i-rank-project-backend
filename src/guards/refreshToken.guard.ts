dfabf771b1ae6ec84e8b9b6125689e75b355f753
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-06-30 19:31:27 +0200

    erase unnecesary code

--- src/guards/refreshToken.guard.ts
+++ src/guards/refreshToken.guard.ts
@@ -1,8 +1,6 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
-// import { IS_PUBLIC_KEY } from 'src/auth/decorators/public.decorator';
 
 @Injectable()
 export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
